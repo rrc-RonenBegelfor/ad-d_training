bcfd1dc90727443c132ad597fa3acd82d7d793d7
Ronen Begelfor <rbegelfor@rrc.ca>
2026-05-08 15:58:29 -0500

    Worked with arrow functions and returning values.

--- script.js
+++ script.js
@@ -18,3 +18,21 @@ const speakFunction = function(name = "Ponen", time = "Morning") {
 
 speakFunction();
 speakFunction("Ronen", "Afternoon");
+
+// returning values (regular function)
+const calcArea = function(radius) {
+    // let area = 3.14 * radius**2;
+    // return area;
+    return 3.14 * radius**2;
+}
+
+// or place in a variable
+console.log(calcArea(5));
+
+// arrow function
+// side note, didnt' actually know this, if there is only one param, you can remove the ().
+const calcAreaArrow = (radius) => {
+    return 3.14 * radius**2;
+}
+
+console.log(calcAreaArrow(2));
