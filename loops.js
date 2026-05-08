147b796916325ec2a638ae8f42bbc0668f55d714
Ronen Begelfor <rbegelfor@rrc.ca>
2026-05-08 14:24:34 -0500

    Completed loops review.

---
+++ loops.js
@@ -0,0 +1,37 @@
+/* for loops */
+// for (let i = 0; i < 5; i++) {
+//     console.log('in loop:', i);
+// }
+
+// console.log('loop finished');
+
+// const names = ['ronen', 'ponen', 'lonen'];
+
+// for (let i = 0; i < names.length; i++) {
+//     console.log(i);
+//     console.log(names[i]);
+
+//     let html = `<div>${names[i]}</div>`;
+//     console.log(html);
+// }
+
+/* while loops */
+// let i = 0;
+
+// while (i < 5) {
+//     console.log('in loop: ', i);
+//     i++;
+// }
+
+// while (i < names.length) {
+//     console.log(names[i]);
+//     i++;
+// }
+
+/* do while loops */
+// i = 5
+
+// do {
+//     console.log('val of i is: ', i);
+//     i++;
+// } while(i < 5);
