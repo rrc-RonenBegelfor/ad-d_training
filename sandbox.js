872095d41739f84e8db32e5c6c7143dc25e5f3ff
Ronen Begelfor <rbegelfor@rrc.ca>
2026-05-08 13:20:03 -0500

    All examples attempted.

---
+++ sandbox.js
@@ -0,0 +1,24 @@
+/* Example 1 */
+// alert('hello, world!');
+
+/* Example 2 */
+// console.log(1);
+// console.log(2);
+
+/* Example 3 */
+// let age = 21;
+// let year = 2026;
+
+// console.log(age, year);
+
+// // Overwriting values
+// age = 30
+// console.log(age);
+
+/* Example 4 */
+// const points = 100;
+
+// console.log(points);
+
+// // Cannot re-assign the value of a constant
+// points = 50;
