122b9cf76261476a56e55d9cfb8a83e68cac08df
Ronen Begelfor <rbegelfor@rrc.ca>
2026-05-08 16:02:09 -0500

    Another arrow function.

--- script.js
+++ script.js
@@ -36,3 +36,13 @@ const calcAreaArrow = (radius) => {
 }
 
 console.log(calcAreaArrow(2));
+
+const bill = (products, tax) => {
+    let total = 0;
+    for (let i = 0; i < products.length; i++) {
+        total += products[i] + products[i] * tax;
+    }
+    return total;
+}
+
+console.log(bill([10,15,30], 0.2));
